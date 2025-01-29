2b609f50942a2533b35c575b744f8e5a40cc15a3
jrbigmon <juniorterriaga0@gmail.com>
2025-01-28 22:09:26 -0300

    feat: added transaction in url controller

--- apps/url-shortener/src/url/url.controller.ts
+++ apps/url-shortener/src/url/url.controller.ts
@@ -4,10 +4,14 @@ import { Request, Response } from 'express';
 import { UrlShortenerInputDTO } from './dto/url-shortener.dto';
 import { getBaseUrl } from '../utils/getBaseUrl';
 import { handleException } from '../../../@share/exceptions/handle.exception';
+import { Sequelize } from 'sequelize-typescript';
 
 @Controller('api/urls')
 export class UrlController {
-  constructor(private readonly service: UrlService) {}
+  constructor(
+    private readonly service: UrlService,
+    private readonly sequelize: Sequelize,
+  ) {}
 
   @Post('shorten')
   public async urlShortener(
@@ -15,14 +19,23 @@ export class UrlController {
     @Res() response: Response,
     @Req() request: Request,
   ) {
+    const transaction = await this.sequelize.transaction();
+
     try {
-      const result = await this.service.urlShortener({
-        url: body?.url,
-        serverUrl: getBaseUrl(request),
-      });
+      const result = await this.service.urlShortener(
+        {
+          url: body?.url,
+          serverUrl: getBaseUrl(request),
+        },
+        transaction,
+      );
+
+      await transaction.commit();
 
       return response.status(HttpStatus.CREATED).json(result);
     } catch (error) {
+      await transaction.rollback();
+
       return handleException(error, response);
     }
   }
