b5381745b972e5fd9aad414e3b085206ced3e4e5
jrbigmon <juniorterriaga0@gmail.com>
2025-01-29 23:19:44 -0300

    doc: login route documentation

--- apps/auth/src/login/dto/login.dto.ts
+++ apps/auth/src/login/dto/login.dto.ts
@@ -1,8 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginInputDTO {
+  @ApiProperty({ type: String })
   email: string;
+
+  @ApiProperty({ type: String })
   password: string;
 }
 
 export class LoginOutputDTO {
+  @ApiProperty({ type: String })
   access_token: string;
 }
--- apps/auth/src/login/login.controller.ts
+++ apps/auth/src/login/login.controller.ts
@@ -1,19 +1,24 @@
 import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
-import { LoginInputDTO } from './dto/login.dto';
+import { LoginInputDTO, LoginOutputDTO } from './dto/login.dto';
 import { LoginService } from './login.service';
 import { Response } from 'express';
 import { handleException } from '../../../@share/exceptions/handle.exception';
+import { ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('api/login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'User has been successfully logged in.',
+    type: LoginOutputDTO,
+  })
   async login(@Body() input: LoginInputDTO, @Res() response: Response) {
     try {
       const result = await this.loginService.login(input);
 
-      return response.status(HttpStatus.OK).json(result);
+      return response.status(HttpStatus.CREATED).json(result);
     } catch (error) {
       return handleException(error, response);
     }
