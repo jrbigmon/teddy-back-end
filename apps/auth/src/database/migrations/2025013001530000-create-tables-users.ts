3f9806b4083ff3897c7e1cbba6311bda3ad24808
jrbigmon <juniorterriaga0@gmail.com>
2025-01-30 18:22:21 -0300

    feat: migration users

---
+++ apps/auth/src/database/migrations/2025013001530000-create-tables-users.ts
@@ -0,0 +1,35 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      deleted_at: Sequelize.DATE,
+    });
+  },
+  down: async (queryInterface) => {
+    return await queryInterface.dropTable('users');
+  },
+};
