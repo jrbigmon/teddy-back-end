7158bab767284c75b0a87383ef586b113ea03451
jrbigmon <juniorterriaga0@gmail.com>
2025-01-30 00:11:29 -0300

    feat: added status code and error name in handle exception

--- apps/@share/exceptions/handle.exception.ts
+++ apps/@share/exceptions/handle.exception.ts
@@ -1,4 +1,4 @@
-import { HttpStatus } from '@nestjs/common';
+import { HttpStatus, Logger } from '@nestjs/common';
 import { Response } from 'express';
 import { InvalidDataException } from './invalid-data.expcetion';
 import { UnauthorizedException } from './unauthorized.exception';
@@ -6,33 +6,45 @@ import { DataAlreadySavedException } from './data-already-saved.exception';
 import { NotFoundException } from './not-found.exception';
 
 export function handleException(error: Error, response: Response): Response {
-  console.error(error);
+  const logger = new Logger('HandleException');
+
+  logger.error(error.message);
 
   if (error.name === InvalidDataException.name) {
-    return response
-      .status(HttpStatus.BAD_REQUEST)
-      .json({ message: error.message });
+    return response.status(HttpStatus.BAD_REQUEST).json({
+      message: error.message,
+      statusCode: HttpStatus.BAD_REQUEST,
+      name: error.name,
+    });
   }
 
   if (error.name === UnauthorizedException.name) {
-    return response
-      .status(HttpStatus.UNAUTHORIZED)
-      .json({ message: error.message });
+    return response.status(HttpStatus.UNAUTHORIZED).json({
+      message: error.message,
+      statusCode: HttpStatus.UNAUTHORIZED,
+      name: error.name,
+    });
   }
 
   if (error.name === DataAlreadySavedException.name) {
-    return response
-      .status(HttpStatus.CONFLICT)
-      .json({ message: error.message });
+    return response.status(HttpStatus.CONFLICT).json({
+      message: error.message,
+      statusCode: HttpStatus.CONFLICT,
+      name: error.name,
+    });
   }
 
   if (error.name === NotFoundException.name) {
-    return response
-      .status(HttpStatus.NOT_FOUND)
-      .json({ message: error.message });
+    return response.status(HttpStatus.NOT_FOUND).json({
+      message: error.message,
+      statusCode: HttpStatus.NOT_FOUND,
+      name: error.name,
+    });
   }
 
-  return response
-    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-    .json({ message: 'Internal Server Error' });
+  return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    message: 'Internal Server Error',
+    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+    name: error.name,
+  });
 }
