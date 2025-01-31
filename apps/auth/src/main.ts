822f520ff194b898e448a97e433a9bc55cb3412c
jrbigmon <juniorterriaga0@gmail.com>
2025-01-30 23:18:09 -0300

    feat: global parse and transform before validation dto

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AuthModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   documentFactory(app, [userDocConfig]);
 
