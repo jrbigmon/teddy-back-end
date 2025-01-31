bc27bdf9c83408be9173a9170be23d9a5d77c63e
jrbigmon <juniorterriaga0@gmail.com>
2025-01-30 22:28:32 -0300

    feat: create user validate input

--- apps/auth/src/users/dto/create-user.dto.ts
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserInputDTO {
   @ApiProperty({ type: String })
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
 
