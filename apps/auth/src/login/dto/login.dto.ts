fe471a8bc0745a8b9360b62ece75eba63d8cb2e1
jrbigmon <juniorterriaga0@gmail.com>
2025-01-30 22:27:27 -0300

    feat: login validate input

--- apps/auth/src/login/dto/login.dto.ts
+++ apps/auth/src/login/dto/login.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginInputDTO {
   @ApiProperty({ type: String })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
   @ApiProperty({ type: String })
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
 
